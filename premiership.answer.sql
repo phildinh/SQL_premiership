17f65b20b74da9e811debed114d3b9e8b6582b69
phildinh <dinhthanhtrung2011@gmail.com>
2024-05-16 12:58:06 +1000

    Add files via upload

---
+++ premiership.answer.sql
@@ -0,0 +1,19 @@
+﻿
+Microsoft Visual Studio Solution File, Format Version 12.00
+# SQL Server Management Studio Solution File, Format Version 19.00
+VisualStudioVersion = 15.0.28307.421
+MinimumVisualStudioVersion = 10.0.40219.1
+Global
+	GlobalSection(SolutionConfigurationPlatforms) = preSolution
+		Default|Default = Default|Default
+	EndGlobalSection
+	GlobalSection(ProjectConfigurationPlatforms) = postSolution
+		{15361FE7-A485-40A5-8A25-BD714CBC6B37}.Default|Default.ActiveCfg = Default
+	EndGlobalSection
+	GlobalSection(SolutionProperties) = preSolution
+		HideSolutionNode = FALSE
+	EndGlobalSection
+	GlobalSection(ExtensibilityGlobals) = postSolution
+		SolutionGuid = {784E6996-9A2A-4BAF-B3E0-AE4262934613}
+	EndGlobalSection
+EndGlobal
